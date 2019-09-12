f1e714083ef0c3bc9dc0613e3611025d653189b8
Arun Sasidharan <arunsasidharan121@gmail.com>
2019-09-12 22:15:08 +0200

    add webview

--- Bullseye/About/AboutViewController.swift
+++ Bullseye/About/AboutViewController.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import WebKit
 
 final class AboutViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        
     }
     
     @IBAction func close() {
